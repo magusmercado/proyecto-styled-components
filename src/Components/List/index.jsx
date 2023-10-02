822c76582121f360d5c1acf556790043ee8dbc77
María Agustina Mercado Aragona <mariagusmercado@gmail.com>
2023-10-02 11:26:34 -0300

    Proyecto de utilización de Styled Components

---
+++ src/Components/List/index.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { Box, Btn } from "../UI";
+import { lista } from "../../info";
+import Card from "../Card";
+
+const List = ( ) => {
+    console.log (lista)
+    return <Box>
+        {
+            lista.cargos.map ((cargo, i) => {
+                return <Card key={i} cargo={cargo}>
+
+                    
+                </Card>
+            })
+        }
+        <Btn>Ver más</Btn>
+    </Box>
+
+}
+
+
+export default List;
